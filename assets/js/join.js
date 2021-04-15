5a770f73cc853112141df3d7bb4eb7f6b5b22466
Kevin Avila <kevin.avila@utexas.edu>
2021-04-15 00:43:03 -0500

    Added agreements markup for desktop

--- assets/js/join.js
+++ assets/js/join.js
@@ -1,14 +1,26 @@
 "use strict";
 
-// If Enter key is pressed check the access code
+// Enter key is pressed on mobile
 $("#access-code").keypress(function(event) {
     if (event.which == 13) {
         // Get access code
         var accessCode = $('#access-code').val()
-        if (!accessCode) {
-            accessCode = $('#access-code-desktop').val()
-        }
+        validateAccessCode(accessCode)
+    }
+})
 
+// Enter key is pressed on desktop
+$("#access-code-desktop").keypress(function(event) {
+    if (event.which == 13) {
+        // Get access code
+        var accessCode = $('#access-code-desktop').val()
+        validateAccessCode(accessCode)
+    }
+})
+
+// Validate access code, grab party, and display agreements
+function validateAccessCode(accessCode) {
+    if (accessCode) {
         $(".join").fadeOut(500, function () {
             var partyUrl = '/party/' + accessCode
             $.get(partyUrl, function(response) {
@@ -19,8 +31,10 @@ $("#access-code").keypress(function(event) {
                 $(".join").fadeIn(500)
             })
         })
+    } else {
+        // No access code error message
     }
-});
+}
 
 // Validate agreements have been accepted and show party page
 function letsParty() {
